61f153446479518234fae59e03aa3ce81fb96d9c
Nandaluri <danielnanda98@gmail.com>
2020-05-26 20:14:53 +0200

    final

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-sdafasdfasdfasdfm;
-asd;
-fas;
-df;
-asdf;
-asdf;
-asdf;
-asdf;
+const profile1 = document.querySelector("#prof1");
+const profile2 = document.querySelector("#prof2");
+
+profile1.onclick = function () {
+  window.open("https://www.instagram.com/sriharshakothur/");
+};
+profile2.onclick = function () {
+  window.open("https://www.instagram.com/rorossi1997/");
+};
